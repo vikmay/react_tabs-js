ca097ed66543c50cf624d3734a857b964d5181b6
Viktor Maikut <vikmaykut@gmail.com>
2024-12-09 19:11:16 +0200

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tabs } from './components/Tabs';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './App.scss';
 
-export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+export const App = () => {
+  const [activeTabId, setActiveTabId] = useState('tab-1');
+
+  const tabs = [
+    { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+    { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+    { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  ];
+
+  const handleTabSelected = id => {
+    setActiveTabId(id);
+  };
+
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
   return (
@@ -9,7 +25,7 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
       <Tabs
         tabs={tabs}
         activeTabId={activeTabId}
-        onTabSelected={onTabSelected}
+        onTabSelected={handleTabSelected}
       />
     </div>
   );
